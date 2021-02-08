28e36c7dfb9cd008e3866635d29412918b59ed5a
Javi Sastre <javisastrecal@gmail.com>
2021-02-08 19:40:15 +0100

    finished lab

--- starter-code/routes/celebrities.js
+++ starter-code/routes/celebrities.js
@@ -1,53 +1,80 @@
-
 const Celebrity = require("./../models/celebrity");
 
-const express = require("express")
+const express = require("express");
 const router = express.Router();
 
 // GET /celebrities/
-router.get('/', (req, res, next)=> {
-    Celebrity.find() 
-    .then( allCelebrities => {
-        console.log('Retrieved all celebrities', allCelebrities)
-        const data = {
-            celebs : allCelebrities
-        }
-        console.log(data, 'holhola')
-    res.render("celebrities/index", data)
-    }
-    )
-    .catch(error =>{
-        console.log(error);
-        next(error);
+router.get("/", (req, res, next) => {
+  Celebrity.find()
+    .then((allCelebrities) => {
+      const data = {
+        celebs: allCelebrities,
+      };
+      res.render("celebrities/index", data);
     })
-})
+    .catch((error) => {
+      console.log(error);
+      next(error);
+    });
+});
 
-router.get('/search/:id', (req, res, next) =>{
-    Celebrity.findById(req.params.id)
+router.get("/search/:id", (req, res, next) => {
+  Celebrity.findById(req.params.id)
     .then((foundCelebrity) => {
-    res.render("celebrities/show", foundCelebrity)
+      res.render("celebrities/show", foundCelebrity);
     })
-    .catch(error =>{
-        console.log(error);
-        next(error);
-    })
-})
+    .catch((error) => {
+      console.log(error);
+      next(error);
+    });
+});
 
 router.get("/new", (req, res, next) => {
-    res.render("celebrities/new")
-})
+  res.render("celebrities/new");
+});
 
-router.post('/new', (req, res, next) =>{
-const { name, occupation, catchPhrase} = req.body;
-Celebrity.create({name, occupation, catchPhrase})
-.then((createdCelebrity) => {
-    res.redirect("/celebrities")
-})
-.catch(error => console.log(error))
+router.post("/create", (req, res, next) => {
+  const { name, occupation, catchPhrase } = req.body;
+  Celebrity.create({ name, occupation, catchPhrase })
+    .then(() => {
+      res.redirect("/celebrities");
+    })
+    .catch((error) => console.log(error));
+});
 
-})
+router.post("/:id/delete", (req, res, next) => {
+  Celebrity.findByIdAndRemove(req.params.id)
+    .then(() => {
+      res.redirect("/celebrities/");
+    })
+    .catch((err) => console.log(err));
+});
 
+router.get("/:id/edit", (req, res, next) => {
+  const { id } = req.params;
+  Celebrity.findById(id)
+    .then((oneCeleb) => {
+      const data = { oneCeleb: oneCeleb };
+      res.render("celebrities/edit", data);
+    })
+    .catch((err) => {
+      console.log(err);
+      next();
+    });
+});
 
-module.exports= router;
+router.post("/:id/edit", (req, res, next) => {
+  const data = {
+    id: req.params.id,
+    name: name,
+    occupation: occupation,
+    catchPhrase: catchPhrase,
+  };
+  Celebrity.findByIdAndUpdate(req.params.id, data)
+    .then(() => {
+      res.redirect("/celebrities");
+    })
+    .catch((err) => console.log(err));
+});
 
-
+module.exports = router;
